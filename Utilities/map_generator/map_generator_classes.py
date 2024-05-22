f1d4887f4b5a5d9a4ca02177e8e9834603d30724
Wojciech Kośnik-Kowalczuk <245728@student.pwr.edu.pl>
2024-05-22 22:31:29 +0200

    Update map generator to fix buggy crossings

--- Utilities/map_generator/map_generator_classes.py
+++ Utilities/map_generator/map_generator_classes.py
@@ -15,7 +15,7 @@ import typing
 #   |
 #   |
 #   v y
-@dataclass
+@dataclass(frozen=True)
 class Coord:
     x: int
     y: int
@@ -23,16 +23,20 @@ class Coord:
     def __add__(self, other):
         if isinstance(other, Coord):
             return Coord(self.x + other.x, self.y + other.y)
-        elif isinstance(other, (int, float)):  # Allows addition with a number to both x and y
+        
+        elif isinstance(other, (int, float)):  
             return Coord(self.x + other, self.y + other)
         else:
+
             return NotImplemented
 
     def __sub__(self, other):
         if isinstance(other, Coord):
             return Coord(self.x - other.x, self.y - other.y)
-        elif isinstance(other, (int, float)):  # Allows subtraction with a number from both x and y
+        
+        elif isinstance(other, (int, float)):
             return Coord(self.x - other, self.y - other)
+        
         else:
             return NotImplemented
 
@@ -45,7 +49,7 @@ class Map:
     # possible tile contents (string):
     # ' ' (empty): grass or sand, tower placement possible
     # 'x' (obsticle): tower placement not possible
-    # '#' (path): tower placement not possible
+    # '^', 'v', '<', '>', '+' (path): tower placement not possible
     # 'S' (start): : tower placement not possible
     # 'E' (end): : tower placement not possible
     # '*' (tower): : tower placement not possible
@@ -105,14 +109,15 @@ class Map:
     def random_map(cls,
                   tileset_path : str,
                   screen : pygame.Surface,
-                  obsticle_density : int = 5, 
+                  obsticle_density : int = 5,
+                  path_length : int = 500,
                   resolution : int = (1920, 1080), 
                   tile_size : int = 60) -> typing.Self:
         
         # Initialize with grass
         board = cls.grass_map(tileset_path, screen, resolution, tile_size)
 
-        # Set initial path
+        # Set path
         board.start = Coord(randint(5, board.grid_size.x - 5), randint(5, board.grid_size.y - 5))
         board.grid[board.start.y][board.start.x] = 'S'  # Mark the starting point
         board.tiles[board.start.y][board.start.x] = board.tileset["path hor"]
@@ -124,39 +129,66 @@ class Map:
         board.tiles[board.start.y - 1][board.start.x - 1] = board.tileset["slope l"]
         board.tiles[board.start.y - 1][board.start.x + 1] = board.tileset["slope r"]
         current_pos = Coord(board.start.x, board.start.y)
-        temp_path = [current_pos]
+        path = [current_pos]
+        # Set first path to go straight out of cave
         current_pos += Coord(0, 1)
-        temp_path.append(current_pos)
-
+        board.grid[current_pos.y][current_pos.x] = 'v'
+        path.append(current_pos)
 
         # Generate random path
-        directions = [Coord(0, 1), Coord(1, 0), Coord(0, -1), Coord(-1, 0)]
+        # Initialize usefull varibles
+        directions = [Coord(0, 1), Coord(1, 0), Coord(0, -1), Coord(-1, 0)]  # Down, Right, Up, Left
+        direction_symbols = {Coord(0, 1): 'v', Coord(1, 0): '>', Coord(0, -1): '^', Coord(-1, 0): '<'}
+        perpendicular = {'^': ('<', '>'), 'v': ('<', '>'), '<': ('^', 'v'), '>': ('^', 'v'), }
         last_direction = Coord(0, 1)
-        path_length = 200
 
+        # loop through path tiles and choose the path
         for _ in range(path_length):
-            temp_directions = (directions + [last_direction] * 2)
+            # Enhance the probability of continuing in the same direction
+            temp_directions = [last_direction] * 5 + directions  # Weight the current direction more
             shuffle(temp_directions)
-            
+
             for direction in temp_directions:
-                next_pos = Coord(current_pos.x + direction.x, current_pos.y + direction.y)
+                if direction.x != -last_direction.x or direction.y != -last_direction.y:
+                    next_pos = current_pos + direction
 
-                # if within the map
-                if 0 <= next_pos.x < board.grid_size.x and 0 <= next_pos.y < board.grid_size.y:
+                    # Check if within the map boundaries
+                    if 0 <= next_pos.x < board.grid_size.x and 0 <= next_pos.y < board.grid_size.y:
+                        current_symbol = direction_symbols[direction]
+                        next_symbol = board.grid[next_pos.y][next_pos.x]
 
-                    ####################################################### ten warunek trzeba poprawić
-                    if next_pos != current_pos - last_direction and (\
-                       board.grid[next_pos.y][next_pos.x] == ' ' or \
-                       board.grid[next_pos.y][next_pos.x] == '#'):
-                        temp_path.append(next_pos)
-                        current_pos = next_pos
-                        last_direction = direction
-                        break
+                        # Check if tile is emptty
+                        if next_symbol == ' ':
+                            print(current_pos, next_pos)
 
+                            board.grid[next_pos.y][next_pos.x] = current_symbol
+                            path.append(next_pos)
+                            current_pos = next_pos
+                            last_direction = direction
+                            break
+
+                        # Check if path is perpendicular to the met path and if the next tile is empty
+                        subsequent_pos = next_pos + direction
+                        if 0 <= subsequent_pos.x < board.grid_size.x and 0 <= subsequent_pos.y < board.grid_size.y and \
+                        board.grid[subsequent_pos.y][subsequent_pos.x] == ' ' and \
+                        next_symbol in perpendicular[current_symbol]:
+                            print(current_pos, next_pos, subsequent_pos)
+                            
+                            board.grid[next_pos.y][next_pos.x] = '+'
+                            board.grid[subsequent_pos.y][subsequent_pos.x] = next_symbol
+                            path.append(next_pos)
+                            path.append(subsequent_pos)
+                            current_pos = subsequent_pos
+                            last_direction = direction
+                            break
+            
+            # If no possible move, break
+            else: 
+                break
 
         # Set end-point
-        board.end = temp_path[-1]
-        last = temp_path[-2]
+        board.end = path[-1]
+        last = path[-2]
         if last.x > board.end.x: 
             board.tiles[board.end.y][board.end.x] = board.tileset['chest l']
         if last.x < board.end.x: 
@@ -167,28 +199,20 @@ class Map:
             board.tiles[board.end.y][board.end.x] = board.tileset['chest b']
 
         board.grid[board.end.y][board.end.x] = 'E'
-        #temp_path = [(1,1), (2,1), (3,1), (4,1), (4,2), (4,3), (4,4), (4,5), (4,6), 
-        #             (4,7), (4,8), (4,9), (5,9), (6,9), (7,9), (8,9), (9,9), (9,8), 
-        #             (9,7), (9,6), (9,5), (8,5), (7,5), (6,5), (5,5), (4,5), (3,5), 
-        #             (2,5), (1,5), (1,6), (1,7), (1,8), (2,8), (3,8), (4,8), (5,8),
-        #             (6,8), (7,8), (8,8), (8,9), (8,10), (8,11), (8,12), (8,13)]
-        #temp_path = [Coord(x, y) for x, y in temp_path]
         
-        board.paths.append(temp_path)
+        board.paths.append(path)
 
-        # Mark the path in the grid and update tiles with path
-
-        for i, pos in enumerate(temp_path[1:-1], 1):
-            board.grid[pos.y][pos.x] = '#'
+        # Update tiles with path
+        for i, pos in enumerate(path[1:-1], 1):
             board.tiles[pos.y][pos.x] = board.select_tile_type(i, pos)
 
-
+        
         for row in range(board.grid_size.x):
             for col in range(board.grid_size.y):
                 if board.grid[col][row] == ' ' and randint(1, 100) < obsticle_density:
                     board.grid[col][row] = 'x'
                     board.tiles[col][row] = board.tileset[f"plant {randint(0, 17)}"]
-
+        
 
         return board
     
