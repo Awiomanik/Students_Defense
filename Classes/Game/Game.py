bca7c370d60af32334a3cf3b7c299fa89b90cb8a
Wojciech Kośnik-Kowalczuk <245728@student.pwr.edu.pl>
2024-06-04 00:18:21 +0200

    Put enemy into Enemy folder for consistency. Commented few things in Game class

--- Classes/Game/Game.py
+++ Classes/Game/Game.py
@@ -3,6 +3,9 @@
 
 # IMPORTS
 from ..UI import UI
+from ..Level import Test_Level
+from ..Player import Player
+from ..Tower import Tower_Classes
 from ..Utilities import Coord
 
 
@@ -17,6 +20,10 @@ class Game():
 
         # INITIALIZE HELPER CLASSES
         self.ui = UI.UI()
+        self.towers = Tower_Classes.Tower_Manager() # what are thoose arguments ???
+        self.player = Player.Player("Player_name_placeholder", 1000, self.towers)
+        self.level = Test_Level.Level(1)
+        # enemies menager
 
         # DISPLAY INTRO
         if display_intro:
@@ -27,12 +34,16 @@ class Game():
 
         # LOAD LEVEL
         self.ui.load_lvl()
+        self.level.start_level()
 
         # Main gameplay loop
         running = True
         while running:
+            # ui get input
+            # enemy menager
+            # towwer menager
             # player 
-            # other classes exchange information
+            # level update
             self.ui.update()
 
 
