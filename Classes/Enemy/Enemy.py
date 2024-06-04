62862ce138c50ad526e2f227b303f1209689e000
MartaSt <282244@student.pwr.edu.pl>
2024-06-04 19:59:06 +0200

    Enemy is moving from his position to his destination

--- Classes/Enemy/Enemy.py
+++ Classes/Enemy/Enemy.py
@@ -1,31 +1,39 @@
-#they should have they life, speed, image and position
-#they can move and take damage 
-#at the moment enemy can only move forward
+#they should have they life, speed, image, position and destination 
+#enemy moves from his position (tuple) to destination (tuple)
 
 class Enemy():
-    def __init__(self, life: int, speed: int, image, position):
+    def __init__(self, life: int, speed: int, image, position, destination):
         self.life = life
         self.speed = speed
         self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.center = position
+        self.x, self.y = position 
+        self.position = position  #position in game 
+        self.destination = destination
+        self.direction = self.calculate_direction()
+
+
+    def calculate_direction(self):
+        x_new = self.destination[0] - self.position[0] #coordinates of the direction vector
+        y_new = self.destination[1] - self.position[1]
+        distance = (x_new**2 + y_new**2) ** 0.5 #length of the vector
+        if distance == 0:
+            return (0, 0)
+        return (x_new / distance, y_new / distance) #normalizing the vector
 
     def move(self):
-        self.rect.x += 1
+        if (self.x, self.y) != self.destination:
+            self.x += self.direction[0] * self.speed #adding to x, normalized vector[0]*speed (which is scalar here)
+            self.y += self.direction[1] * self.speed
+            self.position = (self.x, self.y)
 
     def update(self):
         self.move()
 
-    def take_damage(self, damage):
-        self.life -= damage
-        if self.life < 0:
-            self.life = 0
-
-    def is_alive(self):
-        return self.life > 0
-    
     def __str__(self):
-        return f"Enemy(life={self.life}, speed={self.speed})"
+       return f"Enemy(life={self.life}, speed={self.speed}, position=({self.x}, {self.y}))"
+    
+
+    
     
 
 
