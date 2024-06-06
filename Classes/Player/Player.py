24544f3ecf4c61cdeb75641c17a379dc42d931de
Rzur <mathew.rose@onet.pl>
2024-06-06 13:11:55 +0200

    Very minor changes

--- Classes/Player/Player.py
+++ Classes/Player/Player.py
@@ -12,19 +12,18 @@ class Player: # Defines the player entity
         """Returns which of the towers available on the level are affordable"""
         affordable=[]
         for tower in self.avialable_towers:
-            if tw.tower_types[tower][-1] <= self.gold:
+            if tw.Tower.tower_types[tower][-1] <= self.gold:
                affordable.append(tower) 
         return affordable
 
-    def place_tower(self, tower: tw.Tower, x: int, y: int, ) -> bool:
-        if not self.can_afford_tower(tower):
-            print(f"{self.name} cannot afford the {tower.name} tower.")
+    def place_tower(self, tower: tw.Tower, x: int, y: int) -> bool:
+        if  tower in self.affordable_towers():
+            print("You cannot afford this tower.")
             return False
     
         if not self.tower_manager():
-            print(f"Cannot place {tower.name} at position ({x}, {y}).")
+            print("You can't place a tower here")
             return False
-        self.gold -= tower.cost
+        self.gold -= tw.Tower.tower_types[tower][-1]
 
-        print(f"{self.name} placed a {tower.name} tower at ({x}, {y}).")
         return True
